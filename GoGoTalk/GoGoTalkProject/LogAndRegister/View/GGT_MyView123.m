34c3b168f546afd604a2d1599e2f7d13fb7e33fc
XieHenry <291964988@qq.com>
2017-04-27 15:02:45 +0800

    更新。

---
+++ GoGoTalk/GoGoTalkProject/LogAndRegister/View/GGT_MyView123.m
@@ -0,0 +1,21 @@
+//
+//  GGT_MyView123.m
+//  GoGoTalk
+//
+//  Created by XieHenry on 2017/4/27.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import "GGT_MyView123.h"
+
+@implementation GGT_MyView123
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
