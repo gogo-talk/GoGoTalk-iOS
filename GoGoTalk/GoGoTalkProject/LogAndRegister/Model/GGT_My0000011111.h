2946465f8cac676042a8559baecce23d166561f6
辰 <2446417641@qq.com>
2017-04-27 15:44:06 +0800

    ..

---
+++ GoGoTalk/GoGoTalkProject/LogAndRegister/Model/GGT_My0000011111.h
@@ -0,0 +1,13 @@
+//
+//  GGT_My0000011111.h
+//  GoGoTalk
+//
+//  Created by 辰 on 2017/4/27.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGT_My0000011111 : NSObject
+
+@end
---
+++ GoGoTalk/GoGoTalkProject/LogAndRegister/Model/GGT_My0000011111.m
@@ -0,0 +1,13 @@
+//
+//  GGT_My0000011111.m
+//  GoGoTalk
+//
+//  Created by 辰 on 2017/4/27.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import "GGT_My0000011111.h"
+
+@implementation GGT_My0000011111
+
+@end
