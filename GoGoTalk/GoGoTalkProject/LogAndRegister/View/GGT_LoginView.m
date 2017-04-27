1b1eba7c5c5b49d77157cdd63da8123a45b4872c
XieHenry <291964988@qq.com>
2017-04-27 11:38:09 +0800

    pull 项目

---
+++ GoGoTalk/GoGoTalkProject/LogAndRegister/View/GGT_LoginView.m
@@ -0,0 +1,37 @@
+//
+//  GGT_LoginView.m
+//  GoGoTalk
+//
+//  Created by XieHenry on 2017/4/27.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import "GGT_LoginView.h"
+
+@implementation GGT_LoginView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUpContentView];
+    }
+    return self;
+}
+
+
+- (void)setUpContentView {
+//    self.phoneAccountField = [[UITextField alloc]init];
+//    self.phoneAccountField.placeholder = @"请输入手机账号";
+//    [self addSubview:self.phoneAccountField];
+//    
+//    [self.phoneAccountField mas_makeConstraints:^(MASConstraintMaker *make) {
+//        
+//        
+//    }];
+    
+}
+
+
+
+
+@end
