df5ebb4f963adb7b9371a8aa21d2df4086524d7e
XieHenry <291964988@qq.com>
2017-04-27 15:45:39 +0800

    Ceshi

---
+++ GoGoTalk/GoGoTalkProject/LogAndRegister/Model/GGT_111111.h
@@ -0,0 +1,13 @@
+//
+//  GGT_111111.h
+//  GoGoTalk
+//
+//  Created by XieHenry on 2017/4/27.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGT_111111 : NSObject
+
+@end
