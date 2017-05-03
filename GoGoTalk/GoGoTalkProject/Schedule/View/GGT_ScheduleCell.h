486337d11bf4610f06cba228236965aa82c28709
辰 <2446417641@qq.com>
2017-05-03 18:33:04 +0800

    更新 增加课表cell

---
+++ GoGoTalk/GoGoTalkProject/Schedule/View/GGT_ScheduleCell.h
@@ -0,0 +1,15 @@
+//
+//  GGT_ScheduleCell.h
+//  GoGoTalk
+//
+//  Created by 辰 on 2017/5/3.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGT_ScheduleCell : UITableViewCell
+
++ (instancetype)cellWithTableView:(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath;
+
+@end
