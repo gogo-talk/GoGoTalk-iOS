f29630396c8ad692ae7d89b2c946e7833e055d71
何建新 <yiyuanhanly@126.com>
2017-05-04 11:32:00 +0800

    引导页、启动页、icon

--- GoGoTalk/GoGoTalkProject/MySettings/Controller/GGT_MineViewController.m
+++ GoGoTalk/GoGoTalkProject/MySettings/Controller/GGT_MineViewController.m
@@ -37,11 +37,29 @@
     UIImageView *userInfoView = [UIImageView new];
     userInfoView.image = UIIMAGE_FROM_NAME(@"beijing_wode_top");
     [self.view addSubview:userInfoView];
+    //头像
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"wudingdan_wode"]];
+//    imageView1.frame = CGRectMake(60,100, 100, 100);
+    imageView1.layer.masksToBounds =YES;
+    imageView1.layer.cornerRadius =32;
+    [imageView1.layer setBorderWidth:5];
+    
+    [imageView1.layer setBorderColor:[[UIColor colorWithRed:207/255.0 green:18/255.0 blue:28/255.0 alpha:1] CGColor]];
+                                      
+    [userInfoView addSubview:imageView1];
+    
+    
     [userInfoView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(0);
         make.left.equalTo(self.view.mas_left).with.offset(0);
         make.right.equalTo(self.view.mas_right).with.offset(0);
         make.height.mas_equalTo(LineH(168));
+    }];
+    [imageView1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(userInfoView.mas_top);
+        make.left.equalTo(userInfoView.mas_left).with.offset(LineX(20));
+        make.width.mas_equalTo(LineW(65));
+        make.height.mas_equalTo(LineH(65));
     }];
 }
 @end
