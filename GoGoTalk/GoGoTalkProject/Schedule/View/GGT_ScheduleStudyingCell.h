9f19026a582a5b409a66abbec55aa7da8ab92bbc
辰 <2446417641@qq.com>
2017-05-05 11:58:21 +0800

    更新  完善课表

---
+++ GoGoTalk/GoGoTalkProject/Schedule/View/GGT_ScheduleStudyingCell.h
@@ -0,0 +1,15 @@
+//
+//  GGT_ScheduleStudyingCell.h
+//  GoGoTalk
+//
+//  Created by 辰 on 2017/5/3.
+//  Copyright © 2017年 XieHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGT_ScheduleStudyingCell : UITableViewCell
+
++ (instancetype)cellWithTableView:(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath;
+
+@end
