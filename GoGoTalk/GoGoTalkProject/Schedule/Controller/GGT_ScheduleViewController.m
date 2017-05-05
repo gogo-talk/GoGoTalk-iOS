09ea30879db16f7c13979edea6151cabf59e0d7d
辰 <2446417641@qq.com>
2017-05-05 17:28:10 +0800

    更新

--- GoGoTalk/GoGoTalkProject/Schedule/Controller/GGT_ScheduleViewController.m
+++ GoGoTalk/GoGoTalkProject/Schedule/Controller/GGT_ScheduleViewController.m
@@ -13,7 +13,6 @@
 #import "GGT_ScheduleNormalCell.h"
 #import "GGT_ScheduleFinishedCanPlayCell.h"
 #import "OYCountDownManager.h"
-#import "GGT_PreviewCoursewareVC.h"
 
 static NSString * const CalendarCellID = @"cell";
 static NSString * const xc_TableViewCellID = @"xc_TableViewCellID";
@@ -502,11 +501,6 @@ static NSString * const xc_TableViewCellID = @"xc_TableViewCellID";
     return 0;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    GGT_PreviewCoursewareVC *vc = [GGT_PreviewCoursewareVC new];
-    [self.navigationController pushViewController:vc animated:YES];
-}
 
 
 @end
