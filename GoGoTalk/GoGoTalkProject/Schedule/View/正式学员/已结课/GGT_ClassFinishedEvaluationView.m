85b4137d82d287989118dac41490496fa847b31b
XieHenry <291964988@qq.com>
2018-04-12 13:58:42 +0800

    更新。

--- GoGoTalk/GoGoTalkProject/Schedule/View/正式学员/已结课/GGT_ClassFinishedEvaluationView.m
+++ GoGoTalk/GoGoTalkProject/Schedule/View/正式学员/已结课/GGT_ClassFinishedEvaluationView.m
@@ -18,7 +18,7 @@
 }
 
 -(void)initUI {
-    //教师满意度
+    
     UILabel *titleLabel1 = [[UILabel alloc]init];
     titleLabel1.text = @"教师满意度";
     titleLabel1.font = SystemFont(12);
@@ -30,12 +30,33 @@
         make.top.equalTo(self.mas_top).with.offset(margin15);
         make.height.mas_offset(15);
     }];
+  
     
     
+    UILabel *titleLabel2 = [[UILabel alloc]init];
+    titleLabel2.text = @"本节课学习效果";
+    titleLabel2.font = SystemFont(12);
+    titleLabel2.textColor = UICOLOR_FROM_HEX(Color999999);
+    [self addSubview:titleLabel2];
+    
+    [titleLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.mas_left).with.offset(margin10);
+        make.top.equalTo(self.mas_top).with.offset(margin15);
+        make.height.mas_offset(15);
+    }];
     
     
+    UILabel *titleLabel3 = [[UILabel alloc]init];
+    titleLabel3.text = @"学员评语";
+    titleLabel3.font = SystemFont(12);
+    titleLabel3.textColor = UICOLOR_FROM_HEX(Color999999);
+    [self addSubview:titleLabel3];
     
-    
+    [titleLabel3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.mas_left).with.offset(margin10);
+        make.top.equalTo(self.mas_top).with.offset(margin15);
+        make.height.mas_offset(15);
+    }];
     
     
     
